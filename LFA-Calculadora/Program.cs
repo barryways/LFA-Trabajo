1001ad9a1363e72de42eb11d98b5efad61469bd6
barryways <93161779+barryways@users.noreply.github.com>
2024-01-20 21:33:11 -0600

    Add structures

---
+++ LFA-Calculadora/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LFA_Calculadora
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Código del programa
+
+            Console.WriteLine("¡Hola, mundo!");
+
+            // Más código...
+
+            // Esperar a que el usuario presione una tecla antes de cerrar la consola
+            Console.WriteLine("Presiona una tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+
+}
